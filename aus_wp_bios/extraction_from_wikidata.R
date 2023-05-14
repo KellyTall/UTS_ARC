0ffed0d32262abdb3079f161ebf622c7d504e844
KellyTall <42193386+KellyTall@users.noreply.github.com>
2023-05-14 16:03:25 +1000

    additions to wikidata extracts

--- aus_wp_bios/extraction_from_wikidata.R
+++ aus_wp_bios/extraction_from_wikidata.R
@@ -22,6 +22,8 @@ library(WikidataQueryServiceR)
 # View(aus_citizens_wd)
 
 
+##this query picks up anyone who has ever had Aus citizenship inc R Murdoch ;-)
+
 aus_citizens_wd <- query_wikidata(
   "SELECT DISTINCT  ?person ?personLabel ?personDescription ?sitelink  WHERE {
     SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
@@ -47,3 +49,210 @@ wp_bio_aus_citizens <- aus_citizens_wd %>%
   
 
 View(wp_bio_aus_citizens)
+
+##governors of NSW
+
+wp_bio_gov_NSW <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P39 ?statement0.
+        ?statement0 (ps:P39) wd:Q1528895.
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+View(wp_bio_NSW_gg)
+
+##governors of VIC
+
+wp_bio_gov_VIC <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P39 ?statement0.
+        ?statement0 (ps:P39) wd:Q1571023
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+##governors of QLD
+
+wp_bio_gov_QLD <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P39 ?statement0.
+        ?statement0 (ps:P39) wd:Q1467097
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+##governors of SA
+
+wp_bio_gov_SA <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P39 ?statement0.
+        ?statement0 (ps:P39) wd:Q1840570
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+##people w People of Australia ID / National Centre of Biography - assuming there will be overalp with Women of Aus list below
+people_of_aus <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P9159 ?statement0.
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+View(people_of_aus)
+
+##people w Australian Dictionary of Biography ID
+aus_dictionary <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P1907 ?statement0.
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+View(aus_dictionary)
+
+##women Australia ID - (national centre of biography / ANU school of history)
+women_aus <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P9244 ?statement0.
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+
+##australian war memorial
+aus_wm_people <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P6713 ?statement0.
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+
+#Australian National Maritime Museum person
+maritime_people <- query_wikidata(
+  "SELECT DISTINCT  ?person ?personLabel ?personDescription ?genderLabel ?sitelink 
+  
+  WHERE {
+    SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }
+    
+    {
+      SELECT DISTINCT ?person WHERE {
+        ?person p:P7769 ?statement0.
+      }
+    }
+      
+      OPTIONAL{?person wdt:P21 ?gender.}       
+   
+    OPTIONAL { ?sitelink schema:about ?person ;
+              schema:inLanguage 'en' ;
+              schema:isPartOf [ wikibase:wikiGroup 'wikipedia' ] }.
+  }"
+)
+
+
